0b07e2d4e83b1d9ecf9747ae21554c7217486fc0
Roman Zakaliak <geralt1530@gmail.com>
2026-05-28 21:46:21 +0300

    feat: refactor image processing pipeline and enhance result handling

--- index.ts
+++ index.ts
@@ -2,19 +2,30 @@ import {
   ImageClassificationOutput,
   pipeline,
   RawImage,
+  ZeroShotImageClassificationOutput,
 } from "@xenova/transformers";
 import { readdir } from "fs/promises";
 import { extname, join } from "path";
 import {
   success,
   error,
-  pipe,
+  pipeResult,
   type Result,
   match,
+  pureResult,
 } from "./src/utils/result.js";
 
 const IMAGES_DIR = "./images";
 const IMAGES_EXTENSIONS = [".jpg", ".jpeg", ".png", ".webp"];
+const CAR_LABELS = [
+  "Nissan Leaf White",
+  "Toyota Camry Blue",
+  "Honda Civic Red",
+  "BMW 3 Series Black",
+  "Mercedes-Benz C-Class Silver",
+  "Ford Mustang Yellow",
+  "Chevrolet Corvette White",
+];
 
 async function getImagesList(): Promise<Result<string[], string>> {
   try {
@@ -28,75 +39,110 @@ async function getImagesList(): Promise<Result<string[], string>> {
   }
 }
 
-async function loadImages(filenames: string[]) {
+async function logProcessingStart(filename: string) {
+  console.log(`Processing image: ${filename} ... `);
+  return success(filename);
+}
+
+async function loadImage(filename: string) {
   try {
-    const images = await Promise.all(
-      filenames.map(async (filename) => {
-        const image = await RawImage.read(join(IMAGES_DIR, filename));
-        return {
-          filename,
-          image,
-        };
-      }),
-    );
-    return success(images);
+    const image = await RawImage.read(join(IMAGES_DIR, filename));
+
+    return success({ filename, image });
   } catch (err) {
     return error(err instanceof Error ? err.message : String(err));
   }
 }
 
-const CAR_LABELS = [
-  "Nissan Leaf White",
-  "Toyota Camry Blue",
-  "Honda Civic Red",
-  "BMW 3 Series Black",
-  "Mercedes-Benz C-Class Silver",
-  "Ford Mustang Yellow",
-  "Chevrolet Corvette White",
-];
-
-async function* recognizeCars(images: { filename: string; image: RawImage }[]) {
+async function performRecognition(image: {
+  filename: string;
+  image: RawImage;
+}) {
   try {
     const model = await pipeline(
       "zero-shot-image-classification",
       "Xenova/clip-vit-base-patch32",
     );
 
-    for (const { filename, image } of images) {
-      const results = await model(image, CAR_LABELS);
-      yield success({ filename, results });
-    }
+    const results = await model(image.image, CAR_LABELS);
+    return success({ filename: image.filename, results });
   } catch (err) {
-    return error(err instanceof Error ? err.message : String(err));
+    const message = err instanceof Error ? err.message : String(err);
+    return error(
+      `Failed to perform recognition on ${image.filename}\n: ${message}`,
+    );
   }
 }
 
-async function runRecognition(images: { filename: string; image: RawImage }[]) {
-  const recognitionResults = recognizeCars(images);
-
-  for await (const recognitionResult of recognitionResults) {
-    match(recognitionResult, printRecognitionResults, (err) => {
-      console.error("Error during car recognition: ", err);
-    });
-  }
-}
-
-async function printRecognitionResults(recognitionResults: {
+async function extractRecognitionResults(data: {
   filename: string;
-  results: ImageClassificationOutput | ImageClassificationOutput[];
+  results:
+    | ZeroShotImageClassificationOutput[]
+    | ZeroShotImageClassificationOutput[][];
 }) {
-  console.log(
-    "Car recognition results for",
-    recognitionResults.filename,
-    ":",
-    recognitionResults.results,
-  );
-}
-async function main() {
-  const result = await pipe(getImagesList(), loadImages);
+  const { filename, results } = data;
+  const topResult = results[0];
 
-  match(result, runRecognition, (err) => {
-    console.error("Error during image loading:", err);
+  console.log(`Image: ${filename}`);
+  if (Array.isArray(topResult)) {
+    topResult.forEach((res) => {
+      console.log(`  Label: ${res.label}, Score: ${res.score.toFixed(4)}`);
+    });
+  } else {
+    console.log(
+      `  Label: ${topResult.label}, Score: ${topResult.score.toFixed(4)}`,
+    );
+  }
+
+  return success({ filename, results: topResult });
+}
+
+type ProcessingPipeResult = (
+  initialData: Result<string, string> | Promise<Result<string, string>>,
+) => Promise<
+  Result<
+    {
+      filename: string;
+      results:
+        | ZeroShotImageClassificationOutput
+        | ZeroShotImageClassificationOutput[];
+    },
+    string
+  >
+>;
+
+function run(processingPipe: ProcessingPipeResult) {
+  return async (images: string[]) => {
+    const results = await Promise.allSettled(
+      images.map((filename) => processingPipe(pureResult(filename))),
+    );
+
+    results
+      .filter(
+        (res) =>
+          res.status === "rejected" ||
+          (res.status === "fulfilled" && !res.value.success),
+      )
+      .forEach((error) => {
+        console.error(
+          error.status === "rejected" ? error.reason : error.value.error,
+          "\n-------------------------------\n\n",
+        );
+      });
+  };
+}
+
+async function main() {
+  const processingPipe = await pipeResult(
+    logProcessingStart,
+    loadImage,
+    performRecognition,
+    extractRecognitionResults,
+  );
+  const imagesResult = await getImagesList();
+
+  match(imagesResult, run(processingPipe), (err) => {
+    console.error("Failed to read images directory:", err);
   });
 }
 
--- src/utils/result.ts
+++ src/utils/result.ts
@@ -12,7 +12,7 @@ export const error = <TError>(error: TError): Result<never, TError> => ({
   error,
 });
 
-export const bind = async <TSuccess, TError, U>(
+export const bindResult = async <TSuccess, TError, U>(
   result: Result<TSuccess, TError>,
   fn: (data: TSuccess) => Result<U, TError> | Promise<Result<U, TError>>,
 ): Promise<Result<U, TError>> => {
@@ -24,53 +24,58 @@ export const bind = async <TSuccess, TError, U>(
   }
 };
 
-export function pipe<A, B, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
-): Promise<Result<B, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<B, TError>>;
 
-export function pipe<A, B, C, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
-): Promise<Result<C, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<C, TError>>;
 
-export function pipe<A, B, C, D, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
-): Promise<Result<D, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<D, TError>>;
 
-export function pipe<A, B, C, D, E, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
   fn4: (input: D) => Promise<Result<E, TError>>,
-): Promise<Result<E, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<E, TError>>;
 
-export function pipe<A, B, C, D, E, F, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, F, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
   fn4: (input: D) => Promise<Result<E, TError>>,
   fn5: (input: E) => Promise<Result<F, TError>>,
-): Promise<Result<F, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<F, TError>>;
 
-export function pipe<A, B, C, D, E, F, G, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, F, G, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
   fn4: (input: D) => Promise<Result<E, TError>>,
   fn5: (input: E) => Promise<Result<F, TError>>,
   fn6: (input: F) => Promise<Result<G, TError>>,
-): Promise<Result<G, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<G, TError>>;
 
-export function pipe<A, B, C, D, E, F, G, H, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, F, G, H, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
@@ -78,10 +83,11 @@ export function pipe<A, B, C, D, E, F, G, H, TError>(
   fn5: (input: E) => Promise<Result<F, TError>>,
   fn6: (input: F) => Promise<Result<G, TError>>,
   fn7: (input: G) => Promise<Result<H, TError>>,
-): Promise<Result<H, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<H, TError>>;
 
-export function pipe<A, B, C, D, E, F, G, H, I, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, F, G, H, I, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
@@ -90,10 +96,11 @@ export function pipe<A, B, C, D, E, F, G, H, I, TError>(
   fn6: (input: F) => Promise<Result<G, TError>>,
   fn7: (input: G) => Promise<Result<H, TError>>,
   fn8: (input: H) => Promise<Result<I, TError>>,
-): Promise<Result<I, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<I, TError>>;
 
-export function pipe<A, B, C, D, E, F, G, H, I, J, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, F, G, H, I, J, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
@@ -103,10 +110,11 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, TError>(
   fn7: (input: G) => Promise<Result<H, TError>>,
   fn8: (input: H) => Promise<Result<I, TError>>,
   fn9: (input: I) => Promise<Result<J, TError>>,
-): Promise<Result<J, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<J, TError>>;
 
-export function pipe<A, B, C, D, E, F, G, H, I, J, K, TError>(
-  result: Promise<Result<A, TError>>,
+export function pipeResult<A, B, C, D, E, F, G, H, I, J, K, TError>(
   fn1: (input: A) => Promise<Result<B, TError>>,
   fn2: (input: B) => Promise<Result<C, TError>>,
   fn3: (input: C) => Promise<Result<D, TError>>,
@@ -117,19 +125,19 @@ export function pipe<A, B, C, D, E, F, G, H, I, J, K, TError>(
   fn8: (input: H) => Promise<Result<I, TError>>,
   fn9: (input: I) => Promise<Result<J, TError>>,
   fn10: (input: J) => Promise<Result<K, TError>>,
-): Promise<Result<K, TError>>;
+): (
+  initialData: Promise<Result<A, TError>> | Result<A, TError>,
+) => Promise<Result<K, TError>>;
 
-export async function pipe<TError>(
-  result: Promise<Result<any, TError>>,
+export function pipeResult<TError>(
   ...fns: Array<(data: any) => Promise<Result<any, TError>>>
-): Promise<Result<any, TError>> {
-  let current = await result;
-
-  for (const fn of fns) {
-    current = await bind(current, fn);
-  }
-
-  return current;
+) {
+  return async (initialData: any): Promise<Result<any, TError>> => {
+    return fns.reduce(async (acc, fn) => {
+      const result = await acc;
+      return bindResult(result, fn);
+    }, Promise.resolve(initialData));
+  };
 }
 
 export function match<TSuccess, TError>(
@@ -142,4 +150,21 @@ export function match<TSuccess, TError>(
   } else {
     void onError(result.error);
   }
+}
+
+export function mapResult<TSuccess, TError, U>(
+  result: Result<TSuccess, TError>,
+  fn: (data: TSuccess) => U,
+): Result<U, TError> {
+  if (result.success) {
+    return success(fn(result.data));
+  } else {
+    return result;
+  }
+}
+
+export function pureResult<TSuccess, TError>(
+  data: TSuccess,
+): Result<TSuccess, TError> {
+  return success(data);
 }
